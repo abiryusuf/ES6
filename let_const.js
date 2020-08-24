75cabc86f19d4958fd02af4b40548cddc9bef4f6
abiryusuf <yusuf_ctg@yahoo.com>
2020-08-24 15:48:56 -0500

    letconst

---
+++ let_const.js
@@ -0,0 +1,13 @@
+const name = "abir";
+//name = "yusuf;" // I can not change the value with const key word only array list can change the value. 
+console.log(name);
+const num = [6,7,8];
+num[1] = 10;
+console.log(num);
+
+let sum = 0;
+for (var i = 0; i < 10; i++) {
+    sum = sum + i;
+    //console.log(i)
+}
+console.log(i);

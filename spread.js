5a50b1dea851504edf04d2206d7e66ff0ae356c7
abiryusuf <yusuf_ctg@yahoo.com>
2020-08-25 12:33:14 -0500

    spread opreator

---
+++ spread.js
@@ -0,0 +1,18 @@
+const num1 = 700;
+const num2 = 800;
+const num3 = 600;
+const maxNumber = [num3, num2, num1]
+
+const max = Math.max(num2, num1, num3);
+console.log(max);
+
+console.log("spread operator", Math.max(...maxNumber));
+
+
+function spreadOperator(x, y, z){
+    const maxNumber = [x, y, z];
+    return Math.max(...maxNumber);
+}
+
+const result = spreadOperator(4,6,2);
+console.log(result);
